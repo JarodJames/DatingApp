db2207665ff4a69fc943d697e7f91644a3fdda44
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 18:54:09 +0200

    End of section 8

---
+++ API/Extentions/DatetimeExtentions.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Extentions
+{
+    public static class DatetimeExtentions
+    {
+        public static int CalculateAge(this DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dateOfBirth.Year;
+            if(dateOfBirth.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
